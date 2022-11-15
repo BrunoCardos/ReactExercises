345e423c383dc37a52a7f2fa6641f25b25ada47e
Bruno Cardoso <brunocardoso@sapo.cv>
2022-11-15 11:12:22 -0100

    add+

--- src/components/phoneBook/PhoneBook.js
+++ src/components/phoneBook/PhoneBook.js
@@ -7,15 +7,15 @@ function PhoneBook(){
 
     const [phoneList, setPhoneList] = useState ([
         {
-            "name": "pedro",
+            "name": "Pedro",
             "phone": 9364852,
         },
         {
-            "name": "admin",
+            "name": "Paulo",
             "phone": 5931576,
         },
         {
-            "name": "tech89",
+            "name": "Manuel",
             "phone": 9762415,
         }
     ])
@@ -24,11 +24,12 @@ function PhoneBook(){
     
     const nameInput = useRef ();
     const phoneInput = useRef();
+    const searchInput = useRef();
 
     function addPhone(){
         
-        const name = nameInput.current.value
-        const phone = phoneInput.current.value
+        const name = nameInput.current.value;
+        const phone = phoneInput.current.value;
 
         const newContact = 
             {
@@ -37,40 +38,52 @@ function PhoneBook(){
             }
         
 
-        phoneList.push(newContact) 
+        phoneList.push(newContact); 
 
-        setPhoneList([...phoneList])
+        setPhoneList([...phoneList]);
        
        
         
     }
 
-    return <>
+    function search(){
+        const [searchResult, setSearchResult] = useState ('')
+        let searchName = searchInput.current.value;
+    }
+
+
+    return <div className="phone">
         <div className="left">
+        <h2>Register Number</h2>
             <label>Name:</label> <br/>
-            <input type='text' ref={nameInput}></input>  <br/>
+            <input type='text'  ref={nameInput}></input>  <br/>
             <label>Phone Number:</label> <br/>
             <input type='number' ref={phoneInput}></input> <br/>
-            <button onClick={addPhone}>Add</button>
+            <button onClick={addPhone}>Add Number</button>
         </div>
 
         <div className="right">
-            {phoneList.map((item, i) => <div>
-                name: {item.name} <br/>
-                phone: {item.phone}</div>)
-                
-                }
+            <h2>Phone List</h2>
+            <div className="flex">
+                <input type="search" placeholder="Search" ref={searchInput}></input>
+                <button onClick={search}>Search</button>
+            </div>
+            {searchResult.length==0 && phoneList.map((item, i) => <div><li>
+                Name: {item.name} <br/>
+                Phone: {item.phone}</li></div>)
+              }
+
+            {searchResult.length>0 && phoneList.map((item, i) => <div><li>
+                Name: {item.name} <br/>
+                Phone: {item.phone}</li></div>)
+              }
         </div>
        
-       
-      
-
-     
 
        
     
 
-    </>
+    </div>
 }
 
 export default PhoneBook
