52fba7c612938490f7d508168b20f11602505828
Bruno Cardoso <brunocardoso@sapo.cv>
2022-11-16 10:10:39 -0100

    add+

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,20 +2,24 @@
 
 function Table(props){
 
- return (  props.list.map((item, i)=>
+ return (  
         <table>
             <tr>
                 <th>First Name </th>
                 <th>Last Name</th>
                 <th>Gender</th>
             </tr>
+
+            {props.list.map((item, i)=>
+
             <tr>
                 <td>{item.firstName}</td>
                 <td>{item.lastName}</td>
                 <td>{item.gender}</td>
             </tr>
-        </table>
-    )
+       
+    )}
+    </table>
     )
 }
 
